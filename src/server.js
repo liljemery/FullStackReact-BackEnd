f6f01d54c72ad2e7786a0fc84f809a3e1c80560f
Jemery Inoa <jeremyinoa67@gmail.com>
2023-07-26 09:58:14 -0400

    build: removes cors headers and add middleware

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 import express from "express";
 import {db, DBconnection} from './db.js'
+import cors from 'cors'
 
 const  app = express();
 app.use(express.json());
+app.use(cors());
 var port = 8000;
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5174');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization')
-    next();
-});
 
 app.get('/api/articles/:name', async (req,res)=>{
     const {name} = req.params;
